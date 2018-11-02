99c5d3b083edb9919c1870b34a539477686b14fb
Raghav Pratap <raghavvishnoi10@gmail.com>
2018-11-02 20:14:43 +0530

    Replace after filter to after action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  after_filter :set_header_for_iframe
+  after_action :set_header_for_iframe
 
   private
   	def set_header_for_iframe 
