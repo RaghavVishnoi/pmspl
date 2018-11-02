05d8f1bb352772be90c633daf180891bde7fb2ec
Raghav Pratap <raghavvishnoi10@gmail.com>
2018-11-02 20:11:55 +0530

    Set X-Frame_option to header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  after_filter :set_header_for_iframe
+
+  private
+  	def set_header_for_iframe 
+  		response.headers.delete "X-Frame-Options" 
+  	end 
 end
